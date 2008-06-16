71607b88a29a7a8d8502c5c5e03acf016126ced7
mankeyrabbit <mankeyrabbit@b63810fd-744e-0410-bb97-ef0cbae2f942>
2008-06-16 14:09:01 +0000

    Created genesis branch

---
+++ branches/genesis/src/common.h
@@ -0,0 +1,16 @@
+// common.h -- Defines typedefs and some global functions.
+//             From JamesM's kernel development tutorials.
+
+#ifndef COMMON_H
+#define COMMON_H
+
+// Some standard typedefs, to standardise sizes across platforms.
+// These typedefs are written for 32-bit X86.
+typedef unsigned int   uint32_t;
+typedef          int   int32_t;
+typedef unsigned short uint16_t;
+typedef          short int16_t;
+typedef unsigned char  uint8_t;
+typedef          char  int8_t;
+
+#endif // COMMON_H
---
+++ branches/genesis/src/main.c
@@ -0,0 +1,10 @@
+// main.c -- Defines the C-code kernel entry point, calls initialisation routines.
+//           Made for JamesM's tutorials <www.jamesmolloy.co.uk>
+
+#include "multiboot.h"
+#include "common.h"
+
+int main(multiboot_t *mboot_ptr)
+{
+  return 0xdeadbeef;
+}
